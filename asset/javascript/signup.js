6293f7fcfb97495719865004da34f6e9bd1c8140
yutaka kato <yutakakato2929@gmail.com>
2022-08-10 01:13:12 +0900

    little change

--- asset/javascript/signup.js
+++ asset/javascript/signup.js
@@ -1,6 +1,6 @@
 var query = location.search.slice(4);
 if (query == "account"){
     alert("The account name is already exist, so you should alter a new accont name.")
-}else{
+}else if (query == "name"){
     alert("The User name is already exist, so you should alter a new user name.")
 }
