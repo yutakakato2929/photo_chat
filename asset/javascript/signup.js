1a5928b4e75982086ae95375d0ee86f8a93e333f
yutaka kato <yutakakato2929@gmail.com>
2022-08-10 01:25:20 +0900

    passwd check

--- asset/javascript/signup.js
+++ asset/javascript/signup.js
@@ -3,4 +3,6 @@ if (query == "account"){
     alert("The account name is already exist, so you should alter a new accont name.")
 }else if (query == "name"){
     alert("The User name is already exist, so you should alter a new user name.")
+}else if (query == "passwd"){
+    alert("Password is not matched. So you should put same passwords.")
 }
--- handler/handlerInsertuser.go
+++ handler/handlerInsertuser.go
@@ -8,23 +8,27 @@ import (
 )
 
 func HandlerInsertuser(w http.ResponseWriter, r *http.Request) {
-	db, err := mysql.Openmysql()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer db.Close()
-	user_ac, err := mysql.GetUserByFlag(r.FormValue("account"), "ACCOUNT", db)
-	user_nm, err := mysql.GetUserByFlag(r.FormValue("name"), "NAME", db)
-	if user_ac.Account == "" && user_nm.Name == "" {
-		hashedPasswd := utility.HashStr(r.FormValue("passwd"), "sha256")
-		err = mysql.InsertUser(r.FormValue("account"), r.FormValue("name"), hashedPasswd, db)
+	if r.FormValue("passwd") == r.FormValue("repasswd") {
+		db, err := mysql.Openmysql()
 		if err != nil {
 			fmt.Println(err)
 		}
-		http.Redirect(w, r, "/signin?pa=success", http.StatusSeeOther)
-	} else if user_ac.Account == "" {
-		http.Redirect(w, r, "/signup?pa=name", http.StatusSeeOther)
+		defer db.Close()
+		user_ac, err := mysql.GetUserByFlag(r.FormValue("account"), "ACCOUNT", db)
+		user_nm, err := mysql.GetUserByFlag(r.FormValue("name"), "NAME", db)
+		if user_ac.Account == "" && user_nm.Name == "" {
+			hashedPasswd := utility.HashStr(r.FormValue("passwd"), "sha256")
+			err = mysql.InsertUser(r.FormValue("account"), r.FormValue("name"), hashedPasswd, db)
+			if err != nil {
+				fmt.Println(err)
+			}
+			http.Redirect(w, r, "/signin?pa=success", http.StatusSeeOther)
+		} else if user_ac.Account == "" {
+			http.Redirect(w, r, "/signup?pa=name", http.StatusSeeOther)
+		} else {
+			http.Redirect(w, r, "/signup?pa=account", http.StatusSeeOther)
+		}
 	} else {
-		http.Redirect(w, r, "/signup?pa=account", http.StatusSeeOther)
+		http.Redirect(w, r, "/signup?pa=passwd", http.StatusSeeOther)
 	}
 }
